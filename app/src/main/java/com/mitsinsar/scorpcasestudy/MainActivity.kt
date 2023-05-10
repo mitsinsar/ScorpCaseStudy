b35c99d4c274bcc4ed8227d0ea45b22b7c5b164d
Mithat Sinan Sari <m.sinan.sari@gmail.com>
2023-05-10 12:13:22 +0300

    🎉 Initial Commit

---
+++ app/src/main/java/com/mitsinsar/scorpcasestudy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mitsinsar.scorpcasestudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
